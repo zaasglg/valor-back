9b3f4077a4c3b9f0017806251bde9705af3d32eb
zaasglg <n4msin@mail.ru>
2025-09-28 02:52:27 +0500

    wefwef

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from api.views import hello_world, register, login, get_countries, transactions_list, transaction_create, update_profile, historial_pagos_list, historial_pagos_create, get_user_info
+from api.views import hello_world, register, login, get_countries, transactions_list, transaction_create, update_profile, historial_pagos_list, historial_pagos_create, get_user_info, refresh_token
 
 
 urlpatterns = [
@@ -36,6 +36,7 @@ urlpatterns = [
     path("api/historial_pagos/", historial_pagos_list, name="historial_pagos_list"),
     path("api/historial_pagos/create/", historial_pagos_create, name="historial_pagos_create"),
     path("api/user/info/", get_user_info, name="get_user_info"),
+    path("api/token/refresh/", refresh_token, name="refresh_token"),
 ]
 
 if settings.DEBUG:
